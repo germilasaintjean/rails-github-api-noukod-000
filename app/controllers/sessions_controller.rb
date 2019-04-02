829b3c79fdcaf12e21575f34a9560d78c3c21286
Saint-Jean Germila <germila@noukod.com>
2019-04-02 17:44:12 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,6 @@ class SessionsController < ApplicationController
   req.body = { 'client_id': '28a1c807579871e3858e', 'client_secret':'58640d2730665771dc3f23eec31041dc4433032d' 'code': code }
   req.headers['Accept'] = 'application/json'
    end
-   body = JSON.parse(response.body)
+    redirect_to create_path
   end
 end
