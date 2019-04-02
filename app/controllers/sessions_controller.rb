ce1a7245d2e27ed993e1b26948f5e31adcce1e8e
Saint-Jean Germila <germila@noukod.com>
2019-04-02 17:55:24 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,15 +2,17 @@ class SessionsController < ApplicationController
   skip_before_action :authenticate_user
 
   def create
-    resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
-      req.params['28a1c807579871e3858e'] = ENV['FOURSQUARE_CLIENT_ID']
-      req.params['58640d2730665771dc3f23eec31041dc4433032d'] = ENV['FOURSQUARE_SECRET']
-      req.params['grant_type'] = 'authorization_code'
-      req.params['redirect_uri'] = "http://localhost:3000/auth"
-      req.params['code'] = params[:code]
+    client_id = ENV['GITHUB_CLIENT_ID']
+    client_secret = ENV['GITHUB_CLIENT_SECRET']
+    code = params['code']
+    resp = Faraday.post 'https://github.com/login/oauth/access_token' do |req|
+      req.body = { 'client_id': client_id, 'client_secret': client_secret, 'code': code }
+      req.headers['Accept'] = 'application/json'
     end
 
     body = JSON.parse(resp.body)
-    session[:token] = body["access_token"]
+    session[:token] = body['access_token']
     redirect_to root_path
   end
+  
+end
